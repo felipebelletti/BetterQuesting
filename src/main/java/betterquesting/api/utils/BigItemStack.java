27844a7647aa90abbd0fcab74053abec6ffa2743
Felipe <40951999+fex0r@users.noreply.github.com>
2024-06-08 19:14:39 -0300

    refactored OreDictionary, OreIngredient, handled forge registries

--- src/main/java/betterquesting/api/utils/BigItemStack.java
+++ src/main/java/betterquesting/api/utils/BigItemStack.java
@@ -4,9 +4,12 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.commons.lang3.StringUtils;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.OreIngredient;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -14,14 +17,14 @@ import java.util.List;
 
 /**
  * Purpose built container class for holding ItemStacks larger than 127. <br>
- * <b>For storage purposes only!
+ * <b>For storage purposes only!</b>
  */
 public class BigItemStack {
-    private static final OreIngredient NO_ORE = new OreIngredient("");
+    private static final Ingredient NO_INGREDIENT = Ingredient.of(ItemStack.EMPTY);
     private final ItemStack baseStack;
     public int stackSize;
     private String oreDict = "";
-    private OreIngredient oreIng = NO_ORE;
+    private Ingredient oreIng = NO_INGREDIENT;
 
     public BigItemStack(ItemStack stack) {
         baseStack = stack.copy();
@@ -38,7 +41,7 @@ public class BigItemStack {
     }
 
     public BigItemStack(@Nonnull Block block, int amount, int damage) {
-        this(Item.getItemFromBlock(block), amount, damage);
+        this(Item.byBlock(block), amount, damage);
     }
 
     public BigItemStack(@Nonnull Item item) {
@@ -50,7 +53,8 @@ public class BigItemStack {
     }
 
     public BigItemStack(@Nonnull Item item, int amount, int damage) {
-        baseStack = new ItemStack(item, 1, damage);
+        baseStack = new ItemStack(item, 1);
+        baseStack.setDamageValue(damage);
         this.stackSize = amount;
     }
 
@@ -62,7 +66,7 @@ public class BigItemStack {
     }
 
     public boolean hasOreDict() {
-        return !StringUtils.isEmpty(this.oreDict) && this.oreIng.getMatchingStacks().length > 0;
+        return !StringUtils.isEmpty(this.oreDict) && this.oreIng.getItems().length > 0;
     }
 
     @Nonnull
@@ -71,13 +75,18 @@ public class BigItemStack {
     }
 
     @Nonnull
-    public OreIngredient getOreIngredient() {
+    public Ingredient getOreIngredient() {
         return this.oreIng;
     }
 
     public BigItemStack setOreDict(@Nonnull String ore) {
         this.oreDict = ore;
-        this.oreIng = ore.length() <= 0 ? NO_ORE : new OreIngredient(ore);
+        if (ore.length() <= 0) {
+            this.oreIng = NO_INGREDIENT;
+        } else {
+            TagKey<Item> tagKey = TagKey.create(ForgeRegistries.Keys.ITEMS, new ResourceLocation(ore));
+            this.oreIng = Ingredient.of(tagKey);
+        }
         return this;
     }
 
@@ -85,21 +94,21 @@ public class BigItemStack {
      * Shortcut method to the CompoundTag in the base ItemStack
      */
     public CompoundTag GetTagCompound() {
-        return baseStack.getTagCompound();
+        return baseStack.getTag();
     }
 
     /**
      * Shortcut method to the CompoundTag in the base ItemStack
      */
     public void SetTagCompound(CompoundTag tags) {
-        baseStack.setTagCompound(tags);
+        baseStack.setTag(tags);
     }
 
     /**
      * Shortcut method to the CompoundTag in the base ItemStack
      */
     public boolean HasTagCompound() {
-        return baseStack.hasTagCompound();
+        return baseStack.hasTag();
     }
 
     /**
@@ -131,7 +140,7 @@ public class BigItemStack {
     @Override
     public boolean equals(Object stack) {
         if (stack instanceof ItemStack) {
-            return baseStack.isItemEqual((ItemStack) stack) && ItemStack.areItemStackTagsEqual(baseStack, (ItemStack) stack);
+            return ItemStack.isSameItemSameTags(baseStack, (ItemStack) stack);
         } else {
             return super.equals(stack);
         }
@@ -140,20 +149,16 @@ public class BigItemStack {
     public BigItemStack(@Nonnull CompoundTag tags) // Can load normal ItemStack NBTs. Does NOT deal with placeholders
     {
         CompoundTag itemNBT = tags.copy();
-        itemNBT.setInteger("Count", 1);
-        if (tags.hasKey("id", 99)) {
-            itemNBT.setString("id", "" + tags.getShort("id"));
-        }
-        this.stackSize = tags.getInteger("Count");
+        itemNBT.putInt("Count", 1);
+        this.stackSize = tags.getInt("Count");
         this.setOreDict(tags.getString("OreDict"));
-        this.baseStack = new ItemStack(itemNBT); // Minecraft does the ID conversions for me
-        if (tags.getShort("Damage") < 0) this.baseStack.setItemDamage(OreDictionary.WILDCARD_VALUE);
+        this.baseStack = ItemStack.of(itemNBT);
     }
 
     public CompoundTag writeToNBT(CompoundTag tags) {
-        baseStack.writeToNBT(tags);
-        tags.setInteger("Count", stackSize);
-        tags.setString("OreDict", oreDict);
+        baseStack.save(tags);
+        tags.putInt("Count", stackSize);
+        tags.putString("OreDict", oreDict);
         return tags;
     }
 }
--- src/main/java/betterquesting/api/utils/ItemComparison.java
+++ src/main/java/betterquesting/api/utils/ItemComparison.java
@@ -1,10 +1,14 @@
 package betterquesting.api.utils;
 
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.nbt.*;
 import org.apache.commons.lang3.StringUtils;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.OreIngredient;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,15 +31,15 @@ public class ItemComparison {
         if (stack1.getItem() != stack2.getItem()) {
             return false;
         }
-        if (!(stack1.getItemDamage() == stack2.getItemDamage() || stack1.getItem().isDamageable() || stack1.getItemDamage() == OreDictionary.WILDCARD_VALUE)) {
+        if (!(stack1.getDamageValue() == stack2.getDamageValue() || stack1.getItem().canBeDepleted() || stack1.getDamageValue() == ItemStack.EMPTY.getDamageValue())) {
             return false;
         }
 
         if (nbtCheck) {
-            if (!partialNBT && !stack1.areCapsCompatible(stack2)) {
+            if (!partialNBT && !ItemStack.isSameItemSameTags(stack1, stack2)) {
                 return false;
             }
-            return CompareNBTTag(stack1.getTagCompound(), stack2.getTagCompound(), partialNBT);
+            return CompareNBTTag(stack1.getTag(), stack2.getTag(), partialNBT);
         }
         return true;
     }
@@ -47,7 +51,7 @@ public class ItemComparison {
         } else if (isEmptyNBT(tag1)) // The opposing tag will always be null at this point if the other already is
         {
             return true;
-        } else if (!(tag1 instanceof NBTPrimitive && tag2 instanceof NBTPrimitive) && tag1.getId() != tag2.getId())
+        } else if (!(tag1 instanceof NumericTag && tag2 instanceof NumericTag) && tag1.getId() != tag2.getId())
             return false; // Incompatible tag types (and not a numbers we can cast)
 
         if (tag1 instanceof CompoundTag && tag2 instanceof CompoundTag) {
@@ -56,15 +60,15 @@ public class ItemComparison {
             ListTag list1 = (ListTag) tag1;
             ListTag list2 = (ListTag) tag2;
 
-            if (list1.tagCount() > list2.tagCount() || (!partial && list1.tagCount() != list2.tagCount())) {
+            if (list1.size() > list2.size() || (!partial && list1.size() != list2.size())) {
                 return false; // Sample is missing requested tags or is not exact
             }
 
             topLoop:
-            for (int i = 0; i < list1.tagCount(); i++) {
+            for (int i = 0; i < list1.size(); i++) {
                 Tag lt1 = list1.get(i);
 
-                for (int j = 0; j < list2.tagCount(); j++) {
+                for (int j = 0; j < list2.size(); j++) {
                     if (CompareNBTTag(lt1, list2.get(j), partial)) {
                         continue topLoop;
                     }
@@ -72,20 +76,20 @@ public class ItemComparison {
 
                 return false; // Couldn't find requested tag in list
             }
-        } else if (tag1 instanceof NBTTagIntArray && tag2 instanceof NBTTagIntArray) {
-            NBTTagIntArray list1 = (NBTTagIntArray) tag1;
-            NBTTagIntArray list2 = (NBTTagIntArray) tag2;
+        } else if (tag1 instanceof IntArrayTag && tag2 instanceof IntArrayTag) {
+            IntArrayTag list1 = (IntArrayTag) tag1;
+            IntArrayTag list2 = (IntArrayTag) tag2;
 
-            if (list1.getIntArray().length > list2.getIntArray().length || (!partial && list1.getIntArray().length != list2.getIntArray().length)) {
+            if (list1.getAsIntArray().length > list2.getAsIntArray().length || (!partial && list1.getAsIntArray().length != list2.getAsIntArray().length)) {
                 return false; // Sample is missing requested tags or is not exact
             }
 
             List<Integer> usedIdxs = new ArrayList<>(); // Duplicate control
 
             topLoop:
-            for (int i = 0; i < list1.getIntArray().length; i++) {
-                for (int j = 0; j < list2.getIntArray().length; j++) {
-                    if (!usedIdxs.contains(j) && list1.getIntArray()[i] == list2.getIntArray()[j]) {
+            for (int i : list1.getAsIntArray()) {
+                for (int j = 0; j < list2.getAsIntArray().length; j++) {
+                    if (!usedIdxs.contains(j) && i == list2.getAsIntArray()[j]) {
                         usedIdxs.add(j);
                         continue topLoop;
                     }
@@ -95,20 +99,20 @@ public class ItemComparison {
             }
 
             return true;
-        } else if (tag1 instanceof NBTTagByteArray && tag2 instanceof NBTTagByteArray) {
-            NBTTagByteArray list1 = (NBTTagByteArray) tag1;
-            NBTTagByteArray list2 = (NBTTagByteArray) tag2;
+        } else if (tag1 instanceof ByteArrayTag && tag2 instanceof ByteArrayTag) {
+            ByteArrayTag list1 = (ByteArrayTag) tag1;
+            ByteArrayTag list2 = (ByteArrayTag) tag2;
 
-            if (list1.getByteArray().length > list2.getByteArray().length || (!partial && list1.getByteArray().length != list2.getByteArray().length)) {
+            if (list1.getAsByteArray().length > list2.getAsByteArray().length || (!partial && list1.getAsByteArray().length != list2.getAsByteArray().length)) {
                 return false; // Sample is missing requested tags or is not exact for non-partial match
             }
 
             List<Integer> usedIdxs = new ArrayList<>(); // Duplicate control
 
             topLoop:
-            for (int i = 0; i < list1.getByteArray().length; i++) {
-                for (int j = 0; j < list2.getByteArray().length; j++) {
-                    if (!usedIdxs.contains(j) && list1.getByteArray()[i] == list2.getByteArray()[j]) {
+            for (byte b : list1.getAsByteArray()) {
+                for (int j = 0; j < list2.getAsByteArray().length; j++) {
+                    if (!usedIdxs.contains(j) && b == list2.getAsByteArray()[j]) {
                         usedIdxs.add(j);
                         continue topLoop;
                     }
@@ -116,12 +120,12 @@ public class ItemComparison {
 
                 return false; // Couldn't find requested integer in list
             }
-        } else if (tag1 instanceof NBTTagLongArray && tag2 instanceof NBTTagLongArray) {
-            NBTTagLongArray list1 = (NBTTagLongArray) tag1;
-            NBTTagLongArray list2 = (NBTTagLongArray) tag2;
+        } else if (tag1 instanceof LongArrayTag && tag2 instanceof LongArrayTag) {
+            LongArrayTag list1 = (LongArrayTag) tag1;
+            LongArrayTag list2 = (LongArrayTag) tag2;
 
-            final long[] la1 = NBTConverter.readLongArray(list1);
-            final long[] la2 = NBTConverter.readLongArray(list2);
+            final long[] la1 = list1.getAsLongArray();
+            final long[] la2 = list2.getAsLongArray();
 
             if (la1.length > la2.length || (!partial && la1.length != la2.length)) {
                 return false; // Sample is missing requested tags or is not exact for non-partial match
@@ -140,15 +144,15 @@ public class ItemComparison {
 
                 return false; // Couldn't find requested integer in list
             }
-        } else if (tag1 instanceof NBTTagString && tag2 instanceof NBTTagString) {
+        } else if (tag1 instanceof StringTag && tag2 instanceof StringTag) {
             return tag1.equals(tag2);
-        } else if (tag1 instanceof NBTPrimitive && tag2 instanceof NBTPrimitive) // Standardize numbers to not care about format
+        } else if (tag1 instanceof NumericTag && tag2 instanceof NumericTag) // Standardize numbers to not care about format
         {
             Number num1 = NBTConverter.getNumber(tag1);
             Number num2 = NBTConverter.getNumber(tag2);
 
-            // Check if floating point precesion needs to be preserved in comparison
-            if (tag1 instanceof NBTTagFloat || tag1 instanceof NBTTagDouble || tag2 instanceof NBTTagFloat || tag2 instanceof NBTTagDouble) {
+            // Check if floating point precision needs to be preserved in comparison
+            if (tag1 instanceof FloatTag || tag1 instanceof DoubleTag || tag2 instanceof FloatTag || tag2 instanceof DoubleTag) {
                 return num1.doubleValue() == num2.doubleValue();
             } else {
                 return num1.longValue() == num2.longValue();
@@ -169,10 +173,10 @@ public class ItemComparison {
             return true;
         }
 
-        for (String key : reqTags.getKeySet()) {
-            if (!sample.hasKey(key)) {
+        for (String key : reqTags.getAllKeys()) {
+            if (!sample.contains(key)) {
                 return false;
-            } else if (!CompareNBTTag(reqTags.getTag(key), sample.getTag(key), partial)) {
+            } else if (!CompareNBTTag(reqTags.get(key), sample.get(key), partial)) {
                 return false;
             }
         }
@@ -181,26 +185,26 @@ public class ItemComparison {
     }
 
     private static boolean isEmptyNBT(Tag tag) {
-        return tag == null || tag.isEmpty();
+        return tag == null || tag.toString().isEmpty();
     }
 
     @Deprecated
     public static boolean OreDictionaryMatch(String name, ItemStack stack) {
-        return stack != null && !StringUtils.isEmpty(name) && new OreIngredient(name).apply(stack);
+        return stack != null && !StringUtils.isEmpty(name) && Ingredient.of(TagKey.create(ForgeRegistries.Keys.ITEMS, new ResourceLocation(name))).test(stack);
     }
 
     @Deprecated
     public static boolean OreDictionaryMatch(String name, CompoundTag tags, ItemStack stack, boolean nbtCheck, boolean partialNBT) {
-        if (!nbtCheck) return stack != null && !StringUtils.isEmpty(name) && new OreIngredient(name).apply(stack);
-        return OreDictionaryMatch(new OreIngredient(name), tags, stack, nbtCheck, partialNBT);
+        if (!nbtCheck) return stack != null && !StringUtils.isEmpty(name) && Ingredient.of(TagKey.create(ForgeRegistries.Keys.ITEMS, new ResourceLocation(name))).test(stack);
+        return OreDictionaryMatch(Ingredient.of(TagKey.create(ForgeRegistries.Keys.ITEMS, new ResourceLocation(name))), tags, stack, nbtCheck, partialNBT);
     }
 
     /**
      * Check if the item stack is part of the ore dictionary listing with the given ore ingredient while also comparing NBT tags
      */
-    public static boolean OreDictionaryMatch(OreIngredient ore, CompoundTag tags, ItemStack stack, boolean nbtCheck, boolean partialNBT) {
+    public static boolean OreDictionaryMatch(Ingredient ore, CompoundTag tags, ItemStack stack, boolean nbtCheck, boolean partialNBT) {
         if (stack == null || ore == null) return false;
-        return ore.apply(stack) && (!nbtCheck || CompareNBTTagCompound(stack.getTagCompound(), tags, partialNBT));
+        return ore.test(stack) && (!nbtCheck || CompareNBTTagCompound(stack.getTag(), tags, partialNBT));
     }
 
     /**
@@ -217,19 +221,21 @@ public class ItemComparison {
     public static boolean AllMatch(ItemStack stack1, ItemStack stack2, boolean nbtCheck, boolean partialNBT) {
         if (stack1 == stack2) return true; // Both null or same instance
         if (stack1 == null) return false; // One is null the other is not
-        if (nbtCheck && !CompareNBTTagCompound(stack1.getTagCompound(), stack2.getTagCompound(), partialNBT))
+        if (nbtCheck && !CompareNBTTagCompound(stack1.getTag(), stack2.getTag(), partialNBT))
             return false; // NBT check failed
         if (StackMatch(stack1, stack2, false, false))
             return true; // Stacks are compatible (NBT was already checked at this point)
 
         // Final Ore Dictionary test...
-        int[] oreIDs1 = OreDictionary.getOreIDs(stack1);
-        int[] oreIDs2 = OreDictionary.getOreIDs(stack2);
+        ResourceLocation key1 = ForgeRegistries.ITEMS.getKey(stack1.getItem());
+        ResourceLocation key2 = ForgeRegistries.ITEMS.getKey(stack2.getItem());
 
-        // Search all ore dictionary listings for matches
-        for (int i1 : oreIDs1) {
-            for (int i2 : oreIDs2) {
-                if (i1 == i2) return true; // Shared ore dictionary entries found
+        if (key1 != null && key2 != null) {
+            TagKey<Item> tag1 = TagKey.create(ForgeRegistries.Keys.ITEMS, key1);
+            TagKey<Item> tag2 = TagKey.create(ForgeRegistries.Keys.ITEMS, key2);
+
+            if (tag1 != null && tag2 != null && tag1 == tag2) {
+                return true; // Shared tag found
             }
         }
 
