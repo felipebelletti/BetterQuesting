a882a34a633f84cdbc671c191748294d5570e4ff
Felipe <40951999+fex0r@users.noreply.github.com>
2024-06-08 21:55:04 -0300

    refactored RgbTexture.java TextureUtil.uploadTextureImageAllocate -> DynamicTexture and NativeImage

--- src/main/java/betterquesting/api2/supporter/RgbTexture.java
+++ src/main/java/betterquesting/api2/supporter/RgbTexture.java
@@ -1,19 +1,19 @@
 package betterquesting.api2.supporter;
 
+import com.mojang.blaze3d.platform.NativeImage;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.texture.AbstractTexture;
-import net.minecraft.client.renderer.texture.TextureUtil;
-import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.server.packs.resources.ResourceManager;
 
 import javax.annotation.Nonnull;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
 import java.io.IOException;
 
 public class RgbTexture extends AbstractTexture {
     private final int[] rgbAry;
-
     private final int w;
     private final int h;
+    private DynamicTexture dynamicTexture;
 
     public RgbTexture(int w, int h, int[] rgbAry) {
         this.w = w;
@@ -22,12 +22,38 @@ public class RgbTexture extends AbstractTexture {
     }
 
     @Override
-    public void loadTexture(@Nonnull IResourceManager resourceManager) throws IOException {
-        this.deleteGlTexture();
+    public void load(@Nonnull ResourceManager resourceManager) throws IOException {
+        if (this.dynamicTexture != null) {
+            this.dynamicTexture.close();
+        }
 
-        BufferedImage bufImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        final int[] imgData = ((DataBufferInt) bufImg.getRaster().getDataBuffer()).getData();
-        System.arraycopy(rgbAry, 0, imgData, 0, imgData.length);
-        TextureUtil.uploadTextureImageAllocate(this.getGlTextureId(), bufImg, false, false);
+        NativeImage nativeImage = new NativeImage(w, h, false);
+        nativeImage.upload(0, 0, 0, false);
+
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                nativeImage.setPixelRGBA(x, y, rgbAry[y * w + x]);
+            }
+        }
+
+        this.dynamicTexture = new DynamicTexture(nativeImage);
+        this.bind();
+        this.dynamicTexture.upload();
+    }
+
+    @Override
+    public void bind() {
+        if (RenderSystem.isOnRenderThreadOrInit()) {
+            this.dynamicTexture.bind();
+        } else {
+            RenderSystem.recordRenderCall(this::bind);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (this.dynamicTexture != null) {
+            this.dynamicTexture.close();
+        }
     }
 }
