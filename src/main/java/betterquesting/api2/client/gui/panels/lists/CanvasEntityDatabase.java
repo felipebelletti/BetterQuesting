984a1e9148d2775421284438b97230219ffbe5a3
Felipe <40951999+fex0r@users.noreply.github.com>
2024-06-08 21:12:25 -0300

    refactor: handled ForgeRegistries and EntityType on CanvasEntityDatabase

--- src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasEntityDatabase.java
+++ src/main/java/betterquesting/api2/client/gui/panels/lists/CanvasEntityDatabase.java
@@ -3,15 +3,15 @@ package betterquesting.api2.client.gui.panels.lists;
 import betterquesting.api2.client.gui.controls.PanelButtonStorage;
 import betterquesting.api2.client.gui.misc.GuiRectangle;
 import betterquesting.api2.client.gui.misc.IGuiRect;
-import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class CanvasEntityDatabase extends CanvasSearch<EntityEntry, EntityEntry> {
+public class CanvasEntityDatabase extends CanvasSearch<EntityType<?>, EntityType<?>> {
     private final int btnId;
 
     public CanvasEntityDatabase(IGuiRect rect, int buttonId) {
@@ -20,30 +20,35 @@ public class CanvasEntityDatabase extends CanvasSearch<EntityEntry, EntityEntry>
     }
 
     @Override
-    protected Iterator<EntityEntry> getIterator() {
-        List<EntityEntry> list = new ArrayList<>(ForgeRegistries.ENTITIES.getValuesCollection());
-        list.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+    protected Iterator<EntityType<?>> getIterator() {
+        List<EntityType<?>> list = new ArrayList<>(ForgeRegistries.ENTITY_TYPES.getValues());
+        list.sort((o1, o2) -> o1.getDescriptionId().compareToIgnoreCase(o2.getDescriptionId()));
         return list.iterator();
     }
 
     @Override
-    protected void queryMatches(EntityEntry ee, String query, final ArrayDeque<EntityEntry> results) {
-        if (ee == null || ee.getRegistryName() == null) return;
+    protected void queryMatches(EntityType<?> entityType, String query, final ArrayDeque<EntityType<?>> results) {
+        if (entityType == null || ForgeRegistries.ENTITY_TYPES.getKey(entityType) == null) {
+            return;
+        }
 
         String qlc = query.toLowerCase();
+        String registryName = ForgeRegistries.ENTITY_TYPES.getKey(entityType).toString().toLowerCase();
+        String entityName = entityType.getDescriptionId().toLowerCase();
 
-        if (ee.getRegistryName().toString().toLowerCase().contains(qlc) || ee.getName().toLowerCase().contains(qlc)) {
-            results.add(ee);
+        if (registryName.contains(qlc) || entityName.contains(qlc)) {
+            results.add(entityType);
         }
     }
 
     @Override
-    protected boolean addResult(EntityEntry ee, int index, int cachedWidth) {
-        if (ee == null) {
+    protected boolean addResult(EntityType<?> entityType, int index, int cachedWidth) {
+        if (entityType == null) {
             return false;
         }
 
-        this.addPanel(new PanelButtonStorage<>(new GuiRectangle(0, index * 16, cachedWidth, 16, 0), btnId, ee.getName(), ee));
+        String entityName = entityType.getDescriptionId();
+        this.addPanel(new PanelButtonStorage<>(new GuiRectangle(0, index * 16, cachedWidth, 16, 0), btnId, entityName, entityType));
 
         return true;
     }
